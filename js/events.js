6be57dc99f2d0f600870855f8cc9585643e23fe0
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-03 17:56:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,17 +1,18 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  });
+}
+
+function frameIt(){
+  $(document).on('load', function(){
+    $('img').addClass("tasty")
+  });
+}
 
 $(document).ready(function(){
 // call functions here
-  function getIt(){
-    $('p').on('click', function(){
-      alert("Hey!")
-    });
-  }
 
-  function frameIt(){
-    $(document).on('load', function(){
-      $('img').addClass("tasty")
-    });
-  }
 
 });
